d50de62fecf83cc67d4ab52648c22ffdb6e50860
Prasanta Roy Choudhury <officialprasantaroychoudhury@gmail.com>
2021-05-28 18:58:30 +0530

    new component added

--- src/Components/Sidebar/PriceSlider.js
+++ src/Components/Sidebar/PriceSlider.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const PriceSlider = ({ price, changePrice }) => {
-    return (
-        <div>price</div>
-    )
+
+export default class Slider extends React.Component {
+    state = {
+        value: 0
+    }
+    handleOnChange = (e) => this.setState({ value: e.target.value })
+    render() {
+        return (
+            <React.Fragment>
+                <input type="range" min={0} max={this.props.price} value={this.state.value} onChange={this.handleOnChange} />
+                <div className="value">{this.state.value}</div>
+            </React.Fragment>
+        )
+    }
 }
 
-export default PriceSlider;
