03832076ca4bcf88691c8266bc159afeb2551db9
rishabdhar12 <rishabdhar12@gmail.com>
2021-06-17 19:31:50 +0530

    cart

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Products from "./Pages/Products";
 import SingleProduct from "./Pages/SingleProduct";
+import Cart from "./Pages/Cart";
 import Error from "./Pages/Error";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
@@ -18,6 +19,7 @@ function App() {
         <Route exact path="/about" component={About} />
         <Route exact path="/products" component={Products} />
         <Route exact path="/products/:id" component={SingleProduct} />
+        <Route exact path="/cart" component={Cart} />
         <Route exact component={Error} />
       </Switch>
       <Footer />
