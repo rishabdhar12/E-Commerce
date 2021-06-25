62bcc105a1e55ad4c4fbf621af86766bc23fcccb
Prasanta Roy Choudhury <officialprasantaroychoudhury@gmail.com>
2021-06-25 18:33:45 +0530

    css and structuring Single Product page

--- src/Pages/SingleProduct/index.js
+++ src/Pages/SingleProduct/index.js
@@ -5,6 +5,9 @@ import { Link, useParams } from 'react-router-dom';
 import products from '../../data/data';
 import { useCart } from 'react-use-cart';
 
+//css
+import './single_product.css';
+
 const SingleProduct = () => {
   const [product, setProduct] = useState([]);
   const [amount, setAmount] = useState(1);
@@ -35,49 +38,57 @@ const SingleProduct = () => {
     return;
   }
   return (
-    <div className="single-page-container" key={id}>
-      {/* back to product page */}
-      <Link to='/products' className='single-page-btn'>Back To Products</Link>
+    <main>
+      <div className="sp-container page" key={id}>
 
-      {/* images */}
-      <section>
-        <img src={product.img1} alt="" />
-        <div className="Gallery">
-          <img src={product.img1} alt="" />
-          <img src={product.img2} alt="" />
-        </div>
-      </section>
-      {/* description, availability, freeshiping, company name  */}
-      <section className="content">
-        {/* product name and price */}
-        <div className="">
-          <h2>{product.name}</h2>
-          <h5>${product.price}</h5>
-        </div>
-        <p className="desc">{product.decription}</p>
-        <p><span>Available:</span>
-          {
-            product.available ? "In Stock" : "Not In Stock"
-          }
-        </p>
-        <p><span> Shiping Charge: </span>{
-          product.freeshiping ? "Freeshiping" : "Shipping Charges apply"
-        }</p>
-        <p><span>Brand: </span>{product.company}</p>
-      </section>
-      {/* add items and amount button */}
-      <div>
-        <span>
-          <button className='amount-btn' onClick={decreaseAmount}>-</button>
-          <h2>{amount}</h2>
-          <button className='amount-btn' onClick={increaseAmount}>+</button>
-        </span>
-        <span>
-          <button className="single-page-btn" onClick={() => addItem(product, amount)}>Add to cart</button>
-        </span>
+        {/* back to product page */}
+        <Link to='/products' className='sp-btn'>Back To Products</Link>
 
+        <article className="sp-center">
+
+          {/* images */}
+          <section className="sp-img">
+            <img className="hero-img" src={product.img1} alt="" />
+            <div className="gallery">
+              <img src={product.img1} alt="" />
+              <img src={product.img2} alt="" />
+            </div>
+          </section>
+
+          {/* product details and buttons*/}
+          <section className="content">
+            <h2>{product.name}</h2>
+            <h5 className="price">${product.price}</h5>
+            <p className="desc">{product.decription}</p>
+            <p className="info"><span>Available:</span>
+              {
+                product.available ? "In Stock" : "Not In Stock"
+              }
+            </p>
+            <p className="info"><span> Shiping Charge: </span>{
+              product.freeshiping ? "Freeshiping" : "Shipping Charges apply"
+            }</p>
+            <p className="info"><span>Brand: </span>{product.company}</p>
+            <hr />
+            {/* add items and amount button */}
+            <div className="sp-cart-btn-container">
+
+              <span className="amount-btn">
+                <button onClick={decreaseAmount}><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path></svg></button>
+                <h2>{amount}</h2>
+                <button onClick={increaseAmount}><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path></svg></button>
+              </span>
+
+              <span>
+                <button className="sp-btn" onClick={() => addItem(product, amount)}>Add to cart</button>
+              </span>
+
+            </div>
+          </section>
+
+        </article>
       </div>
-    </div>
+    </main>
   );
 }
 
